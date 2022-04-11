5515ccfc915a390d4c97c9e9c0dc693dab1aaf94
Hendrik du Toit <hendrik@brightedge.co.za>
2022-04-12 00:23:18 +0200

    PackageIt initial project creation.

---
+++ tests/test_packageitstructureexample.py
@@ -0,0 +1,50 @@
+'''Testing packageitstructureexample__init__()'''
+
+from pathlib import Path
+from beetools.beearchiver import Archiver
+import packageitstructureexample
+
+
+_PROJ_DESC = __doc__.split('\n')[0]
+_PROJ_PATH = Path(__file__)
+
+
+def project_desc():
+    return _PROJ_DESC
+
+
+b_tls = Archiver(_PROJ_DESC, _PROJ_PATH)
+
+
+class TestPackageItStructureExample:
+    def test__init__(self, env_setup_self_destruct):
+        """Assert class __init__"""
+        env_setup = env_setup_self_destruct
+        t_packageitstructureexample = (
+            packageitstructureexample.PackageItStructureExample(
+                "PackageItStructureExample", env_setup.dir
+            )
+        )
+
+        assert t_packageitstructureexample.success
+        pass
+
+    def test_method_1(self, env_setup_self_destruct):
+        """Assert class __init__"""
+        env_setup = env_setup_self_destruct
+        t_packageitstructureexample = (
+            packageitstructureexample.PackageItStructureExample(
+                "PackageItStructureExample", env_setup.dir
+            )
+        )
+
+        assert t_packageitstructureexample.method_1(
+            "THis is a test message for Method_1"
+        )
+        pass
+
+    def test_do_examples(self):
+        packageitstructureexample.do_examples()
+
+
+del b_tls
